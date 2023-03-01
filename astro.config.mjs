3bddb7ee41b2e309ea2dcea8eb26082e4468f514
Julien Bras <julienbras@sidoine.org>
2023-02-28 23:47:24 -0500

    github pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sidoine.org/",
+  site: "https://julbrs.github.io/",
   integrations: [
     tailwind({
       config: {
